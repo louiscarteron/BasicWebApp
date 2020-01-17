87e25a9b09568eed43fe884455e5916e0e630328
Louis Carteron <ljc216@ic.ac.uk>
2020-01-17 17:00:01 +0000

    Update html

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome to this awesome search engine!</h1>" +
                 "<p>Enter your query in the box below and we'll do our best!: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
